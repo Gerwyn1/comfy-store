cea2a2bf0ef300d9ee60023130a9bb4a3d360ad9
gerwyn <gerwynsim@hotmail.com>
2025-08-10 10:33:51 +0800

    guest login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -26,6 +26,24 @@ export const action =
   };
 
 const Login = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const loginAsGuestUser = async () => {
+    try {
+      const response = await customFetch.post("/auth/local", {
+        identifier: "jams122@gmail.com",
+        password: "secret",
+      });
+      dispatch(loginUser(response.data));
+      toast.success("welcome guest user");
+      navigate("/");
+    } catch (error) {
+      console.log(error)
+      toast.error('guest user login error, please try again ')
+    }
+  };
+
   return (
     <section className="h-screen grid place-items-center">
       <Form
@@ -37,18 +55,20 @@ const Login = () => {
           type="email"
           label="email"
           name="identifier"
-          defaultValue="test@test.com"
         />
         <FormInput
           type="password"
           label="password"
-          defaultValue="secret"
           name="password"
         />
         <div className="mt-4">
           <SubmitBtn text="login" />
         </div>
-        <button type="button" className="btn btn-secondary btn-block">
+        <button
+          type="button"
+          className="btn btn-secondary btn-block"
+          onClick={loginAsGuestUser}
+        >
           guest user
         </button>
         <p className="text-center">
